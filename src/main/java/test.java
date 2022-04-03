f9a6e6ddb740d2b1c9de2ea22c614714902980fb
yyyy-cell <83171690+yyyy-cell@users.noreply.github.com>
2022-04-03 16:59:39 +0800

    Update test.java
    thired commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello1");
+        System.out.println("hello2");
     }
 }
